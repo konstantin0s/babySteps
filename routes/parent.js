4c40f978072b0b5a752a2e72000b5c54444b5b1a
konstantin0s <constantintofan85@gmail.com>
2020-07-20 19:49:00 +0200

    Fix checking a parent when logged in as babysitter

--- routes/parent.js
+++ routes/parent.js
@@ -6,13 +6,13 @@ let Parent = require('../models/parent');
 router.get('/parent/:id', async function(req, res) {
     try {
         await Parent.findOne({ _id: req.params.id }, function(err, parent) {
-            if (res.locals.family) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    res.render('parent', { parent: parent, family: req.session.family, sitter: req.session.sitter, layout: false });
-                }
+
+            if (err) {
+                console.log(err);
+            } else {
+                res.render('parent', { parent: parent, family: req.session.family, sitter: req.session.sitter, layout: false });
             }
+
         });
     } catch (error) {
         console.log(error);
