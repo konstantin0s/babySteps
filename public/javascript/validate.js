eaf877fa5b2c0f4c6a30f46c6002c4938fd2834b
konstantin0s <constantintofan85@gmail.com>
2020-08-02 20:20:06 +0200

    Add check validation on input fields on signup sitter form

--- public/javascript/validate.js
+++ public/javascript/validate.js
@@ -1,4 +1,5 @@
 function emailValidation() {
+
     const signUpForm = document.getElementById('form');
     const emailField = document.getElementById('email');
     const okButton = document.getElementById('submit');
@@ -15,3 +16,56 @@ function emailValidation() {
 }
 
 window.onload = emailValidation;
+
+function validateRequiredField() {
+
+    const okButton = document.getElementById('submit');
+    const signUpForm = document.getElementById('form');
+
+    if (signUpForm.checkValidity()) {
+        okButton.disabled = false;
+    } else {
+        okButton.disabled = true;
+    }
+}
+validateRequiredField();
+minimLengthInput();
+
+function minimLengthInput() {
+    let help = document.getElementById('help');
+    let fields = document.querySelectorAll('input');
+    for (let field of Array.from(fields)) {
+        field.addEventListener('focus', (event) => {
+            let text = event.target.getAttribute('data-help');
+            if (help) {
+                help.textContent = text;
+                validateInput();
+                if (field >= 3) {
+                    field.addEventListener('change', (event) => {
+                        help.textContent = '';
+                    });
+                }
+            }
+            if (help.textContent === null) {
+
+                console.log('lalaal')
+            }
+
+        });
+        field.addEventListener('blur', (event) => {
+            help.textContent = '';
+        });
+    }
+}
+
+function validateInput() {
+    const okButton = document.getElementById('submit');
+    if (this.value < 3) {
+        alert('minum 3 ')
+        okButton.disabled = false;
+        // this.style.cssText = "border-width:5px;border-color:red;border-style:solid;border-radius:3px;";
+    } else {
+        okButton.disabled = true;
+        // this.style.cssText = "border-width:5px;border-color:limegreen;border-style:solid;border-radius:3px;";
+    }
+}
